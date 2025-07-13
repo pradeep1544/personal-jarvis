5509e31f0d7efa6492016088cd4996bd5c1ec265
pradeep1544 <hanumanthupradeep8@gmail.com>
2025-07-13 23:41:45 +0530

    Create music.py

---
+++ music.py
@@ -0,0 +1,6 @@
+music = {
+    "ring":"https://youtu.be/bLx0NDUIrL8?si=_-_pdx3-PfW6GSME",
+    "skyfall":"https://youtu.be/DeumyOzKqgI?si=QJ8VcK_ap87g5L0r",
+    "kingdom":"https://youtu.be/8eb6T14AxEY?si=i2-sl8Am3wIFhbqn",
+    "kill":"https://youtu.be/NQjiHbdWQOc?si=n2dCcD6gHvaYaDuq"
+}
